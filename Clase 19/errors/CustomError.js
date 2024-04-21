533d6cbb21cc1614941034402d3795c2f6d20bfc
José Miranda Valderrama <joseduardomiranda10@gmail.com>
2024-04-21 20:28:47 -0300

    Se inicia desafío 21 y elimina carpetas de desafíos ya corregidos

--- Clase 19/errors/CustomError.js
+++
@@ -1,9 +0,0 @@
-export class CustomError {
-    static createError({name='Error', cause, message, code=5}) {
-        let error = new Error(message)
-        error.cause = cause  
-        error.name = name
-        error.code = code
-        throw error
-    }
-}
